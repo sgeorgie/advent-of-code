3948d0a305a59e9e9ab9708c1d0770ee0e3cb505
Sarmad Georgie <sgeorgie@terkwaz.com>
2020-12-06 20:38:16 +0200

    Solve 2020, day 6

---
+++ 2020/AdventOfCode.2020.Day6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var groups = File.ReadAllText("input.txt")
+                .Split("\n\n")
+                .Select(group => group
+                    .Split("\n")
+                    .Select(person => person.ToCharArray())
+                    .ToArray())
+                .ToArray();
+
+            // Part 1
+            var totalAnswers = 0;
+
+            foreach (var group in groups)
+            {
+                totalAnswers += group
+                    .SelectMany(person => person)
+                    .ToHashSet()
+                    .Count;
+            }
+
+            Console.WriteLine($"Part 1: Sum of unique yes answers in all groups is {totalAnswers}");
+
+            // Part 2
+            var totalCommonAnswers = 0;
+
+            foreach (var group in groups)
+            {
+                IEnumerable<char> commonAnswers = group.First();
+
+                for (int i = 1; i < group.Length; i++)
+                {
+                    var person = group[i];
+                    commonAnswers = commonAnswers.Intersect(person);
+                }
+
+                totalCommonAnswers += commonAnswers.Count();
+            }
+
+            Console.WriteLine($"Part2: Sum of common answers in all groups is {totalCommonAnswers}");
+        }
+    }
+}
